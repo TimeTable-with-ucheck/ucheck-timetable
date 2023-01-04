70ae5b440d226ab8c8880d102b4b573c01f4ae84
leejongmin <60211689@mju.ac.kr>
2023-01-04 18:56:05 +0900

    2023/01/04 기능 업데이트

--- app/src/main/java/com/example/test/ScheduleSearch.java
+++ app/src/main/java/com/example/test/ScheduleSearch.java
@@ -1,4 +1,52 @@
 package com.example.test;
 
-public class ScheduleCSV {
+import android.content.res.AssetManager;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class ScheduleSearch {
+    AssetManager assetManager;
+    public ScheduleSearch(AssetManager assetManager){
+        this.assetManager = assetManager;
+    }
+
+
+    public JSONObject search(String subjectNum) {
+        System.out.println(subjectNum+" in search");
+
+        try {
+            InputStream inputStream = assetManager.open("ScheduleData/schedule.json");
+            InputStreamReader isr = new InputStreamReader(inputStream);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuffer buffer= new StringBuffer();
+            String line = reader.readLine();
+            while(line != null){
+                buffer.append(line+"\n");
+                line = reader.readLine();
+            }
+            JSONArray jsonArray = new JSONArray(buffer.toString());
+            System.out.println("드디어 제이슨 어레이는 길이 "+jsonArray.length()+"에 도달했다");
+            for(int i = 0 ; i<jsonArray.length();i++){
+                JSONObject element = (JSONObject) jsonArray.opt(i);
+                System.out.println("log--"+element.optString("강좌번호"));
+                if(element.optString("강좌번호").equals(subjectNum)) {
+                    System.out.println("마참내");
+                    return element;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("제기랄");
+        return null;
+    }
 }
